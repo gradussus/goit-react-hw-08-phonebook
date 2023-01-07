3eeeed06a10b218f64ffe628e67a9ff1f8787699
Vitalii Shevchenko <shevchenkovitaliy20@gmail.com>
2023-01-07 13:11:53 +0200

    Update UserMenu.jsx

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,15 +1,17 @@
-import { useDispatch, useSelector } from "react-redux"
-import { getUserName, getUserEmail } from "redux/auth/authSelectors"
-import {logout} from '../../redux/auth/authOperations'
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserName, getUserEmail } from 'redux/auth/authSelectors';
+import { logout } from '../../redux/auth/authOperations';
 
 export const UserMenu = () => {
-    const dispatch = useDispatch()
-    const name = useSelector(getUserName)
-    const email = useSelector(getUserEmail)
-    return (<div>
-        <span>
-            {`Hello, ${name} (${email})`} 
-        </span>
-    <button type="button" onClick={() => dispatch(logout())}>LogOut</button></div>
-    )
-}
+  const dispatch = useDispatch();
+  const name = useSelector(getUserName);
+  const email = useSelector(getUserEmail);
+  return (
+    <div>
+      <span>{`Hello, ${name} (${email})`} </span>
+      <button type="button" onClick={() => dispatch(logout())}>
+        LogOut
+      </button>
+    </div>
+  );
+};
