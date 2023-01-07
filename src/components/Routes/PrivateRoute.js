14a640c561aac613334e16c0fd2dbd13cdb22bca
Vitalii Shevchenko <shevchenkovitaliy20@gmail.com>
2023-01-07 22:28:24 +0200

    fix

--- src/components/Routes/PrivateRoute.js
+++ src/components/Routes/PrivateRoute.js
@@ -1,13 +1,8 @@
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
-import { getIsLoggedIn, getIsRefreshing } from 'redux/auth/authSelectors';
+import { getIsLoggedIn } from 'redux/auth/authSelectors';
 
 export const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
   const isLoggedIn = useSelector(getIsLoggedIn);
-  const isRefreshing = useSelector(getIsRefreshing);
-  return isLoggedIn && isRefreshing ? (
-    <Component />
-  ) : (
-    <Navigate to={redirectTo} />
-  );
+  return isLoggedIn ? <Component /> : <Navigate to={redirectTo} />;
 };
